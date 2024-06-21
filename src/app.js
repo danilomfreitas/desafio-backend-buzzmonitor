659973204bad0cac895d2e2b58d83430d87f22d8
Danilo de Menezes <42979803+danilomfreitas@users.noreply.github.com>
2024-06-21 16:54:32 -0300

    Creating app and setting up server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const livroRoute = require('./routes/livroRoute');
+
+app.use('/livros', livroRoute);
+
+module.exports = app;
